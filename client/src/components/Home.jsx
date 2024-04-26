0fb5930cda917f5b949e93ef15b3602eaa66ddd7
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-04-26 00:26:56 -0300

    Algunos componentes creados para mi React + Vite + Redux. Los estilos de Tailwind no funcionan.

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+// src/components/Home.js o src/components/Home.jsx
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi aplicación</h1>
+      <p>Esta es la página de inicio.</p>
+    </div>
+  );
+};
+
+export default Home;
