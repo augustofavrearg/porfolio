24e0706681188924c4aa4b9be62302a88cc63399
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-04-26 22:52:58 -0300

    Navbar bien. Footer mal.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,9 @@
 
 
-const FooterComponent = () => {
-  return (
-    <section className="bg-black pt-12 pb-4 shadow dark:bg-gray-900 m-4">
-      <div className="w-full max-w-screen-xl mx-auto px-4 md:px-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img className="w-24 h-auto md:w-32 md:h-auto" src="https://static.wixstatic.com/media/39c6da_c313300b528e4aa284d37b4c31f951a8~mv2.png/v1/crop/x_83,y_128,w_336,h_226/fill/w_154,h_104,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Untitled%20design.png" alt="Flowbite Logo" />
-            <span className="self-center text-white text-xl md:text-2xl font-semibold whitespace-nowrap">AIQ TRAINING</span>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-300 sm:mb-0 dark:text-gray-400">
-            
-          </ul>
-        </div>
-        <span className="block text-sm text-gray-300 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">AIQ LEARNING</a>. All Rights Reserved.</span>
-      </div>
-    </section>
-  );
-};
+const Footer = () => {
+    return (
+        <div></div>
+    );
+}
 
-export default FooterComponent;
+export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,46 @@
-// src/components/Navbar.js
+import { useState } from 'react'
+import  styles from './Navbar.module.css';
 
-import React from 'react';
-
-
-const Navbar = () => {
+function Navbar() {
+  // adding the states 
+  const [isActive, setIsActive] = useState(false);
+  //add the active class
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
+  };
+  //clean up function to remove the active class
+  const removeActive = () => {
+    setIsActive(false)
+  }
   return (
-    <div className="bg-gray-800 text-white p-4">
-      <ul className="flex">
-        <li className="mr-6"><a href="/" className="hover:underline">Inicio</a></li>
-        <li className="mr-6"><a href="/acerca" className="hover:underline">Acerca</a></li>
-        <li><a href="/contacto" className="hover:underline">Contacto</a></li>
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <nav className={`${styles.navbar}`}>
+          {/* logo */}
+          <a href='#home' className={`${styles.logo}`}>Dev. </a>
+          <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
+            <li onClick={removeActive}>
+              <a href='#home' className={`${styles.navLink}`}>Home</a>
+            </li>
+            <li onClick={removeActive}>
+              <a href='#home' className={`${styles.navLink}`}>Catalog</a>
+            </li>
+            <li onClick={removeActive}>
+              <a href='#home' className={`${styles.navLink}`}>All products</a>
+            </li>
+            <li onClick={removeActive}>
+              <a href='#home' className={`${styles.navLink}`}>Contact</a>
+            </li>
+          </ul>
+          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+          </div>
+        </nav>
+      </header>
     </div>
   );
-};
-
+}
 export default Navbar;
+;
